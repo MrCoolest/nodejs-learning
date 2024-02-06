bc6ef2d7f06fb51ed095f1820be1d6fef8874a74
Ankit <anktipatwa2141@gmail.com>
2024-02-06 20:34:43 +0530

    2/6/24

---
+++ modules/hello.js
@@ -0,0 +1,10 @@
+// const math = require('./math')
+// console.log("The Addition of two number",math.add(3,6))
+// console.log("The Subtraction of two number",math.sub(3,6))
+
+// Destructuing
+const {add, sub} = require('./math')
+
+console.log("The Addition of two number",add(3,6))
+console.log("The Subtraction of two number",sub(3,6))
+
---
+++ modules/math.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+
+// module.exports = 'Ankit';
+// module.exports.add = add;
+// module.exports= {add, sub};
+
+// exports.add = add;
+// exports.sub = sub;
+exports.add = (a,b) => a+b;
+exports.sub = (a,b) => a-b;
