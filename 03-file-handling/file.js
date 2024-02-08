f070d8e8cf1924904f75eaeddb349a756179d024
Ankit <anktipatwa2141@gmail.com>
2024-02-08 08:53:42 +0530

    2/8/24

--- 03-file-handling/file.js
+++ 03-file-handling/file.js
@@ -5,4 +5,31 @@ const fs = require('fs')
 
 
 // Asyncrounous call
-fs.writeFile()
+// fs.writeFile('./test.txt',"Heloo World Async",(err)=> `Something is wrong ${err}`)
+
+
+// Asyncrounous call
+// fs.readFile("./contact.txt","utf-8",(err,res)=>{
+//     if(err){
+//         console.log('err',err);
+//     }else{
+//         console.log('Async',res);
+//     }
+// })
+
+
+
+// // Scyncrounous call
+// const readValue = fs.readFileSync('./contact.txt','utf-8');
+// console.log('sync',readValue);
+
+
+// // Scyncrounous call
+// fs.appendFileSync("./test.txt", new Date().toLocaleString()+'\n');
+
+
+fs.copyFileSync("./test.txt",'./copy.txt')
+
+// fs.unlinkSync('./copy.txt')
+
+console.log(fs.statSync('./test.txt'))
