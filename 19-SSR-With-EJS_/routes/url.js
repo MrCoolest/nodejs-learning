c980fa8b29064139151b68dc87caf1617dae41cb
Ankit <ankitpatwa2141@gmail.com>
2024-04-27 17:30:39 +0530

    27.4.2023

---
+++ 19-SSR-With-EJS_/routes/url.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { generateShortUrl, getAnlyticsUrl, handelShortUrl } = require('../controllers/url');
+
+const router = express.Router();
+
+router.post('/',generateShortUrl);
+router.get('/anlyticurl/:shortid',getAnlyticsUrl);
+router.get('/:shortId',handelShortUrl);
+
+
+module.exports = router
